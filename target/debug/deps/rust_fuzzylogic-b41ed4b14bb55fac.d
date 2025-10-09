7ff9765cd54f1e74b8cdbe37d7b3f063e8619ca8
JoshuaKento <89899771+JoshuaKento@users.noreply.github.com>
2025-10-10 02:17:25 +0900

    changelog fix

--- target/debug/deps/rust_fuzzylogic-b41ed4b14bb55fac.d
+++ target/debug/deps/rust_fuzzylogic-b41ed4b14bb55fac.d
@@ -1,6 +1,6 @@
-C:\Users\Joshua\Desktop\DevEnv\rust-fuzzylogic\target\debug\deps\rust_fuzzylogic-b41ed4b14bb55fac.d: src\lib.rs src\membership\mod.rs src\membership\gaussian.rs src\membership\trapezoidal.rs src\membership\triangular.rs src\aggregate.rs src\antecedent.rs src\builder.rs src\defuzz.rs src\error.rs src\mamdani.rs src\ops.rs src\rulespace.rs src\sampler.rs src\system.rs src\term.rs src\variable.rs src\prelude.rs
+C:\Users\Joshua\Desktop\DevEnv\rust-fuzzylogic\target\debug\deps\rust_fuzzylogic-b41ed4b14bb55fac.d: src\lib.rs src\membership\mod.rs src\membership\gaussian.rs src\membership\trapezoidal.rs src\membership\triangular.rs src\aggregate.rs src\antecedent.rs src\builder.rs src\defuzz.rs src\error.rs src\mamdani.rs src\ops.rs src\rulespace.rs src\sampler.rs src\term.rs src\variable.rs src\prelude.rs
 
-C:\Users\Joshua\Desktop\DevEnv\rust-fuzzylogic\target\debug\deps\librust_fuzzylogic-b41ed4b14bb55fac.rmeta: src\lib.rs src\membership\mod.rs src\membership\gaussian.rs src\membership\trapezoidal.rs src\membership\triangular.rs src\aggregate.rs src\antecedent.rs src\builder.rs src\defuzz.rs src\error.rs src\mamdani.rs src\ops.rs src\rulespace.rs src\sampler.rs src\system.rs src\term.rs src\variable.rs src\prelude.rs
+C:\Users\Joshua\Desktop\DevEnv\rust-fuzzylogic\target\debug\deps\librust_fuzzylogic-b41ed4b14bb55fac.rmeta: src\lib.rs src\membership\mod.rs src\membership\gaussian.rs src\membership\trapezoidal.rs src\membership\triangular.rs src\aggregate.rs src\antecedent.rs src\builder.rs src\defuzz.rs src\error.rs src\mamdani.rs src\ops.rs src\rulespace.rs src\sampler.rs src\term.rs src\variable.rs src\prelude.rs
 
 src\lib.rs:
 src\membership\mod.rs:
@@ -16,7 +16,6 @@ src\mamdani.rs:
 src\ops.rs:
 src\rulespace.rs:
 src\sampler.rs:
-src\system.rs:
 src\term.rs:
 src\variable.rs:
 src\prelude.rs:
